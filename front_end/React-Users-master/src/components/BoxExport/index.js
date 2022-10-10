9dc38b624b2aeafed9acbbf40beccca3793c50ee
FRED VO <vhnam30@gmail.com>
2022-10-10 21:32:22 +0700

    new

--- front_end/React-Users-master/src/components/BoxExport/index.js
+++ front_end/React-Users-master/src/components/BoxExport/index.js
@@ -1,89 +1,120 @@
-import styles from './BoxExport.module.scss'
-import classNames from 'classnames/bind'
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import React, {useState, useRef, useEffect} from 'react'
+import styles from './BoxExport.module.scss';
+import classNames from 'classnames/bind';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import React, { useReducer } from 'react';
 
+const urlExport = 'http://localhost:9000/products/create-paint-export';
+const urlDetail = 'http://localhost:9000/products/detail-paint-item/';
+const cx = classNames.bind(styles);
+const initState = {
+    productExport: '',
+    productsExport: {},
+    productsExportTotal: {}
+};
 
+const SET_PRODUCT = 'set-product';
+const ADD_PRODUCT = 'add-product';
 
-const cx = classNames.bind(styles)
+const setProduct = (payload) => {
+    return {
+        type: SET_PRODUCT,
+        payload,
+    };
+};
 
-function ButtonExport({props}) {
-    const urlExport = "http://localhost:9000/products/create-paint-export"
-    const urlDetail = "http://localhost:9000/products/detail-paint-item/"
-    const productsId = props
+const addProduct = (payload) => {
+    return {
+        type: ADD_PRODUCT,
+        payload
+    };
+};
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case SET_PRODUCT:
+            return {
+                ...state,
+                productExport: action.payload,
+            };
 
+        case ADD_PRODUCT: {
+            return {
+                ...state,
+                productsExport: [...state.productExport, action.payload],
+            };
+        }
 
-
-
-    const [amountExport, setAmountExport] = useState({
-        amount: "",
-        productsId
-    })
-
-    const [newAmount, setNewAmount] = useState()
-
-    const {amount} = amountExport
-
-    console.log(amountExport)
-
-    const onInputChange = e => {
-        setAmountExport({ ...amountExport , [e.target.name]: e.target.value });
-     };
-
-     
-    
-    
-    const handleSelect = (e) => {
-        e.target.classList.add('hide')
-        const wrapper = e.target.closest('div')
-        wrapper.querySelector('div').classList.add('active')     
-
+        default:
+            throw new Error('Invalid value');
     }
+};
 
+function ButtonExport({ props }) {
+    const id = props;
 
+    const [state, dispatch] = useReducer(reducer, initState);
 
+    const { productExport, productsExport,productsExportTotal } = state;
 
+    console.log(productsExport)
 
+    const handleAdd = () => {
+        dispatch(addProduct(productExport));
+        dispatch(setProduct(''));
+    };
+
+    const handleSelect = (e) => {
+        e.target.classList.add('hide');
+        const wrapper = e.target.closest('div');
+        wrapper.querySelector('div').classList.add('active');
+    };
 
     const handleCancelSelect = (e) => {
-        e.target.classList.add('active')
-        const wrapper = e.target.closest('div')
-        wrapper.querySelector('div').classList.add('hide')  
-    }
+        e.target.classList.add('active');
+        const wrapper = e.target.closest('div');
+        wrapper.querySelector('div').classList.add('hide');
+    };
 
-    return ( <div  className={cx('wrapper')}>
-        <button onClick={handleSelect} id="btn-wrapper" className={cx('btn', 'btn-success')}>
-            Chọn
-        </button>
+    return (
+        <div className={cx('wrapper')}>
+            <button
+                onClick={handleSelect}
+                id="btn-wrapper"
+                className={cx('btn', 'btn-success')}
+            >
+                Chọn
+            </button>
 
-        <div id="input-wrapper" className={cx('input-wrapper')}>
-        <input
+            <div id="input-wrapper" className={cx('input-wrapper')}>
+                <input
+                    type="number"
+                    className={cx('input')}
+                    placeholder="Nhập số lượng"
+                    name="productExport"
+                    value={productExport}
+                    onChange={(e) => {
+                        dispatch(setProduct(e.target.value));
+                    }}
+                />
 
-            type='number'
-            className = {cx('input')}
-            placeholder='Nhập số lượng'
-            name='amount'
-            value={amount}
-            onChange={e => onInputChange(e)}
+                <button
+                    onClick={handleCancelSelect}
+                    className={cx('btn', 'btn-danger')}
+                >
+                    Hủy
+                </button>
 
-        />
-
-        <button onClick = {handleCancelSelect} className={cx('btn', 'btn-danger')}>
-            Hủy
-        </button>
-
-        <button className={cx('btn', 'btn-light')}>Xác nhận</button>
+                <button
+                    onClick={handleAdd}
+                    id="btn"
+                    className={cx('btn', 'btn-light')}
+                >
+                    Xác nhận
+                </button>
+            </div>
         </div>
-
-      
-    </div> )
+    );
 }
 
 export default ButtonExport;
-
-
-
-
-
