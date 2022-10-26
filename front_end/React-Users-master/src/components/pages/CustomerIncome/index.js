2d16f9dae390e5160c32ba3fa713d7ff3086ed7e
FRED VO <vhnam30@gmail.com>
2022-10-26 08:54:14 +0700

    nam

--- front_end/React-Users-master/src/components/pages/CustomerIncome/index.js
+++ front_end/React-Users-master/src/components/pages/CustomerIncome/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styles from './CustomerIncome.module.scss';
 import classNames from 'classnames/bind';
 import axios from 'axios';
+import { Link, NavLink } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -9,8 +10,7 @@ function CustomerIncome() {
     const [phoneNumber, setPhoneNumber] = useState();
 
     const [customerIncome, setCustomerIncome] = useState([]);
-
-    const detailIncome = customerIncome.customerPaintExport
+    const [detailExport, setDetailExport] = useState([]);
 
     const onSubmit = async () => {
         await axios
@@ -20,6 +20,7 @@ function CustomerIncome() {
 
             .then((response) => {
                 setCustomerIncome(response.data);
+                setDetailExport(response.data.customerPaintExport);
             })
 
             .catch(() => {
@@ -29,7 +30,7 @@ function CustomerIncome() {
 
     return (
         <div className={cx('container')}>
-            <div className={cx('w-75 mx-auto', 'shadow p-5')}>
+            <div className={cx('w-75 mx-auto', 'shadow p-5', 'wrapper')}>
                 <h2 className={cx('text-center', 'mb-4')}>
                     Doanh số theo khách hàng
                 </h2>
@@ -52,31 +53,41 @@ function CustomerIncome() {
                 </div>
             </div>
 
-            <div className="container py-4" style={{ marginTop: '40px' }}>
-                <h3 style={{ fontSize: '30px' }}>
-                    Chi tiết khách hàng {customerIncome.full_name}
+            <div
+                className={cx('container', 'py-4')}
+                style={{ marginTop: '40px', marginLeft: '120px' }}
+            >
+                <h3 style={{ fontSize: '30px', paddingBottom: '20px' }}>
+                    Kết quả:
                 </h3>
-                <ul className="list-group w-50">
-                    <li className="list-group-item">
+                <ul className={cx('list-group', 'w-50')}>
+                    <li className={cx('list-group-item')}>
+                        Họ và tên: {customerIncome.full_name}
+                    </li>
+                    <li className={cx('list-group-item')}>
                         Số điện thoại khách hàng: {customerIncome.phone_number}
                     </li>
 
-                    <li className="list-group-item">
+                    <li className={cx('list-group-item')}>
                         Tổng giá trị hàng hóa:{' '}
                         {customerIncome.total_income_customer} VND
                     </li>
 
-                    <li className="list-group-item">
+                    <li className={cx('list-group-item')}>
                         Chi tiết đơn hàng:
                         <ul>
-                            {/* {customerIncome.customerPaintExport.map(
-                                (data, index) => (
-                                    <li key={index}>
-                                        {data.amount} {data.dvt}{' '}
-                                        {data.product_name}
-                                    </li>
-                                ),
-                            )} */}
+                            {detailExport.map((data, index) => (
+                                <li key={index}>
+                                    <Link
+                                        class="btn btn-success"
+                                        to={`/detailbillexport/${data.id}`}
+                                        target="_blank"
+
+                                    >
+                                        Thời gian xuất hóa đơn {data.created_time}
+                                    </Link>
+                                </li>
+                            ))}
                         </ul>
                     </li>
                 </ul>
